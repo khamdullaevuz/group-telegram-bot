873826adaea478211c10732710c554e708843a4f
KhamdullaevUz <elbek.khamdullaev@gmail.com>
2021-06-16 10:08:39 +0500

    edit

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import ChatType
 
 API_TOKEN = '1805310908:AAFe_G34KTXHhlkeM4EYRiLDf4Sry6MHlNI'
 
@@ -20,13 +21,15 @@ button2 = InlineKeyboardButton('Buyruqlar', callback_data="commands")
 main_keyboard = InlineKeyboardMarkup()
 main_keyboard.add(button1, button2)
 
-@dp.message_handler(commands=['start', 'help'])
+@dp.message_handler(chat_type=ChatType.PRIVATE)
 async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
+	if message.text == "/start":
+		await message.reply("Salom men guruhlarda ishlovchi botman!", reply_markup=main_keyboard)
 
-    await message.reply("Salom men guruhlarda ishlovchi botman!", reply_markup=main_keyboard)
+@dp.message_handler(chat_type=[ChatType.SUPERGROUP, ChatType.GROUP])
+async def send_welcome(message: types.Message):
+	if message.text == "/start@ChatSettingsBot":
+		await message.reply("Men guruhda ishlashim uchun admin berishingiz kerak!")
 
 backbutton = InlineKeyboardButton('Ortga', callback_data="back")
 back_keyboard = InlineKeyboardMarkup()
@@ -43,6 +46,11 @@ async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
     text = 'Salom men guruhlarda ishlovchi botman!'
 
     await bot.edit_message_text(text, query.from_user.id, query.message.message_id, reply_markup=main_keyboard)
+
+@dp.message_handler(content_types=["new_chat_member"])
+async def handler_new_member(message):
+    first_name = message.new_chat_member.first_name
+    await bot.send_message(message.chat.id, "ok")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
